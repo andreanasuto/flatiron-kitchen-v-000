93911cb7daaf7d0d51109d8063bbed9662c2df32
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-07 20:53:48 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,20 @@
 class RecipesController < ApplicationController
+
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name).
+  end
+
 end

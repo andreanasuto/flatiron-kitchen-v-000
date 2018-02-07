edf9e354d8c5128f75fd351530da1e9f64856914
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-07 21:02:58 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,19 @@
 class IngredientsController < ApplicationController
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def create
+    @ingredient = Ingredient.create(recipe_params)
+  end
+
+  def recipe_params
+    params.require(:ingredient).permit(:name)
+  end
+
 end
